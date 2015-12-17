49c7172416379c9c30e478f4d60a22cb6fcc3e73
Павел <wolfram@root.root>
2015-12-17 16:31:34 +0200

    change entities

---
+++ src/AppBundle/Entity/Player.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\PlayerRepository")
+ */
+class Player extends User
+{
+
+}
